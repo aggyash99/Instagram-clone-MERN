2bcaf64de5f3d0a971c4c7accd3db8b9fea0cec9
Yash Aggarwal <aggyash99@gmail.com>
2020-07-06 02:45:47 +0530

    Update keys.js

--- merninstagram/server/keys.js
+++ merninstagram/server/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI : "mongodb+srv://dbyash:dwPpmF1gV1yItcgB@cluster0-iuosf.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET : "vbfyivbidvandinvd"
+    MONGOURI : "mongodb",
+    JWT_SECRET : "randomstring"
 }
